1adfbca8de2b666c5998b162c7660fc93782cccd
mayrajp <may.musique@gmail.com>
2023-07-05 12:09:59 -0300

    refix

--- src/Providers/DynamicFormServiceProvider.php
+++ src/Providers/DynamicFormServiceProvider.php
@@ -10,31 +10,15 @@ class DynamicFormServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(str_replace('/src', '/database/migrations', __DIR__));
-        // $this->loadRoutesFrom('/routes/api.php');
-
+        $this->loadMigrationsFrom('/database/migrations');
 
     }
     public function register()
     {
-        // $this->app->register(
-        //     DynamicFormServiceProvider::class,
-        // );  
-
-        // $router = $this->app->make('router');
-
-        // $router ->namespace('Api')
-        //         ->controller(DynamicFormController::class)->group(function () {
-        //             Route::get('/dynamic_forms/all', 'index');
-        //             Route::post('/dynamic_forms/create', 'create');
-        //             Route::get('/dynahttps://packagist.org/packages/mayrajp/forms#v1.0.1mic_forms/show/{id}', 'show');
-        //             Route::put('/dynamic_forms/update/{id}', 'update');
-        //             Route::delete('/dynamic_forms/delete/{id}', 'destroy');
-        //         });
-
         $this->app->make('Mayrajp\Forms\Http\Controllers\Api\CompletedFormController');
         $this->app->make('Mayrajp\Forms\Http\Controllers\Api\DynamicFormController');  
         $this->app->make('Mayrajp\Forms\Http\Controllers\Api\FieldController');
 
     }
+    
 }
