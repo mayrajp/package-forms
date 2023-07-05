a57c866d47495208b0e2f102d216465db1cbe34c
mayrajp <may.musique@gmail.com>
2023-07-05 11:07:29 -0300

    fisrt commit

---
+++ src/Providers/DynamicFormServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mayrajp\Forms\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+
+class FormularioContatoServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+
+    }
+    public function register()
+    {
+        // $this->app->register(
+        //     DynamicFormServiceProvider::class,
+        // );  
+
+        // $router = $this->app->make('router');
+
+        // $router ->namespace('Api')
+        //         ->controller(DynamicFormController::class)->group(function () {
+        //             Route::get('/dynamic_forms/all', 'index');
+        //             Route::post('/dynamic_forms/create', 'create');
+        //             Route::get('/dynamic_forms/show/{id}', 'show');
+        //             Route::put('/dynamic_forms/update/{id}', 'update');
+        //             Route::delete('/dynamic_forms/delete/{id}', 'destroy');
+        //         });
+
+        $this->app->make('Mayrajp\Forms\Http\Controllers\Api\CompletedFormController');
+        $this->app->make('Mayrajp\Forms\Http\Controllers\Api\DynamicFormController');  
+        $this->app->make('Mayrajp\Forms\Http\Controllers\Api\FieldController');
+
+    }
+}
